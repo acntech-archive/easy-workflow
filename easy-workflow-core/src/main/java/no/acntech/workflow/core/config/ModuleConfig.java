a79239c0fcc9497a1f6a0dd09d12b6d3e6299f87
Teo <onlyteo@live.com>
2017-06-11 22:27:57 +0200

    Initial Spring config

---
+++ easy-workflow-core/src/main/java/no/acntech/workflow/core/config/ModuleConfig.java
@@ -0,0 +1,7 @@
+package no.acntech.workflow.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModuleConfig {
+}
---
+++ easy-workflow-engine/src/main/java/no/acntech/workflow/engine/config/ModuleConfig.java
@@ -0,0 +1,7 @@
+package no.acntech.workflow.engine.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModuleConfig {
+}
